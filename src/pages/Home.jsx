ced3827ac6473bbd529448cb2af190308e332fc3
Muhtasir Shafkat <msm.shafkat99@gmail.com>
2023-11-14 13:16:43 +0600

    created plane and sky

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,88 @@
+/* eslint-disable no-unused-vars */
+import { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
+
 const Home = () => {
-	return <div>Home</div>;
+	const [isRotating, setIsRotating] = useState(false);
+	const [currentStage, setCurrentStage] = useState(1);
+	const adjustIslandForScreenSize = () => {
+		let screenScale = null;
+		let screenPositon = [0, -6.5, -43];
+		let rotation = [0.1, 4.7, 0];
+
+		if (window.innerWidth < 768) {
+			screenScale = [0.9, 0.9, 0.9];
+		} else {
+			screenScale = [1, 1, 1];
+		}
+		return [screenScale, screenPositon, rotation];
+	};
+	const adjustPlaneForScreenSize = () => {
+		let screenScale, screenPositon;
+
+		if (window.innerWidth < 768) {
+			screenScale = [1.5, 1.5, 1.5];
+			screenPositon = [0, -1.5, 0];
+		} else {
+			screenScale = [3, 3, 3];
+			screenPositon = [0, -4, -4];
+		}
+		return [screenScale, screenPositon];
+	};
+	const [planeScale, planePosition] = adjustPlaneForScreenSize();
+
+	const [islandScale, islandPosition, islandRotation] =
+		adjustIslandForScreenSize();
+	return (
+		<section className="w-full h-screen relative">
+			<Canvas
+				className={`w-full h-screen bg-transparent ${
+					isRotating ? "cursor-grabbing" : "cursor-grab"
+				}`}
+				camera={{ near: 0.1, far: 1000 }}
+			>
+				<Suspense fallback={<Loader />}>
+					<directionalLight position={[1, 1, 1]} intensity={2} />
+					<ambientLight intensity={0.5} />
+					{/* <pointLight /> */}
+					{/* <spotLight  /> */}
+					<hemisphereLight
+						skyColo="#b1e1ff"
+						groundColor="#000000"
+						intensity={1}
+					/>
+					<Bird />
+
+					<Sky isRotating={isRotating} />
+					<Island
+						position={islandPosition}
+						scale={islandScale}
+						rotation={islandRotation}
+						isRotating={isRotating}
+						setCurrentStage={setCurrentStage}
+						setIsRotating={setIsRotating}
+					/>
+					<Plane
+						isRotating={isRotating}
+						planeScale={planeScale}
+						planePosition={planePosition}
+						rotation={[0, 20, 0]}
+					/>
+				</Suspense>
+			</Canvas>
+		</section>
+	);
 };
 
 export default Home;
+{
+	/* <div className="absolute top-28 left-0 right-0 z-10
+		flex items-center justify-center">
+			POPUP
+		</div> */
+}
